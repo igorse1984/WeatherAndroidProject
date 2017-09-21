32d8cb53bb0d073ca4a397cbbbbd56fe70ad0bf6
Igor <igorse1984@gmail.com>
2017-09-21 03:27:13 +0300

    переход на фрагменты

--- app/src/main/java/ru/igorsharov/weatherapp/MainActivity.java
+++ app/src/main/java/ru/igorsharov/weatherapp/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.igorsharov.weatherapp;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements OneFragment.OneFragmentInterface {
@@ -16,8 +17,12 @@ public class MainActivity extends AppCompatActivity implements OneFragment.OneFr
 
     @Override
     public void onListViewSelected(int pos) {
-        getFragmentManager().beginTransaction().add(R.id.fragment_container, new SecondFragment()).commit();
+// подключаем FragmentManager
 
+        // Получаем ссылку на второй фрагмент по ID
+        SecondFragment secondFragment = new SecondFragment();
+        secondFragment.setWeather(pos);
+        getFragmentManager().beginTransaction().replace(R.id.fragment_container, secondFragment).commit();
     }
 }
 
